7e2face28a8e833075d11a5321527358373438ed
Sile9t <eyesless1jack@gmail.com>
2024-06-18 18:36:34 +0300

    Added ProductController.AddProduct method

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FirstWebApp.Data;
+using FirstWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FirstWebApp.Controllers
 {
@@ -6,6 +8,22 @@ namespace FirstWebApp.Controllers
     [Route("[controller]")]
     public class ProductController : ControllerBase
     {
+        [HttpPost]
+        public ActionResult<int> AddProduct(string name, string description, decimal price)
+        {
+            using (var context = new StorageContext())
+            {
+                if (context.Products.Any(x => x.Name == name))
+                    return StatusCode(409);
+
+                var product = new Product() {Name = name, Desctiption = description, Price = price };
+
+                context.Products.Add(product);
+                context.SaveChanges();
+                return Ok(product.Id);
+            }
+        }
+
 
     }
 }
