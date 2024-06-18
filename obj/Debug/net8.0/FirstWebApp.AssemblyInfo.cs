e65b0fa388c69bb7f71d9dca646ca35af1fd6cae
Sile9t <eyesless1jack@gmail.com>
2024-06-18 15:42:22 +0300

    Added Product.Storages field

--- obj/Debug/net8.0/FirstWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/FirstWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8aa7b1e1dbc4da26bc4c6a5659668ce9abbcae6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
