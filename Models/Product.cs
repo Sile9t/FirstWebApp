f57cffd42a9d1fd1b05e7b7ce6f9c18f787f93c8
Sile9t <eyesless1jack@gmail.com>
2024-06-18 15:33:14 +0300

    Added Product class

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,5 +2,11 @@
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Desctiption { get; set; }
+        public int? GroupId { get; set; }
+        public virtual ProductGroup? Group { get; set; }
     }
 }
--- Models/Storage.cs
+++ Models/Storage.cs
@@ -2,5 +2,11 @@
 {
     public class Storage
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Desctiption { get; set; }
+        public virtual ProductGroup? Group { get; set; }
+        public int GroupId { get; set; }
     }
 }
