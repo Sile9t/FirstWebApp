a4f4297348797a5df87d4e173c96c0c8ffbfc1c5
Sile9t <eyesless1jack@gmail.com>
2024-06-18 15:39:05 +0300

    Added Storage class

--- Models/Storage.cs
+++ Models/Storage.cs
@@ -3,10 +3,8 @@
     public class Storage
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public string Desctiption { get; set; }
-        public virtual ProductGroup? Group { get; set; }
-        public int GroupId { get; set; }
+        public int Count { get; set; }
+        public int? ProductId { get; set; }
+        public Product Product { get; set; }
     }
 }
